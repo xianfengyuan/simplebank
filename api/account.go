c2101b0ccea1f6a003ae40da6ba390ceedf909b9
Xianfeng Yuan <xianyuan@gmail.com>
2025-06-18 16:36:13 -0700

    handle error code

--- api/account.go
+++ api/account.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5/pgconn"
 	db "github.com/xianfengyuan/simplebank/db/sqlc"
 )
 
@@ -28,6 +29,13 @@ func (server *Server) createAccount(ctx *gin.Context) {
 
 	account, err := server.store.CreateAccount(ctx, arg)
 	if err != nil {
+		if pgerr, ok := err.(*pgconn.PgError); ok {
+			switch pgerr.Code {
+			case "23503", "23505":
+				ctx.JSON(http.StatusForbidden, errorResponse(err))
+				return
+			}
+		}
 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
 		return
 	}
