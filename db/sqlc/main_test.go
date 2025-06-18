6317a42fabb61d7bc1439e00fdd85ca2fb3888f7
Xianfeng Yuan <xianyuan@gmail.com>
2025-06-17 20:00:11 -0700

    change postgres url

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -11,7 +11,7 @@ import (
 
 const (
 	dbDriver = "postgres"
-	dbSource = "postgres://xianfengyuan@localhost/simple_bank?sslmode=disable"
+	dbSource = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
 )
 
 var testQueries *Queries
