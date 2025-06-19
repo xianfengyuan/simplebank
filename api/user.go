800ee5adc4edfc92bbde659d8a3a5eefff676619
Xianfeng Yuan <xianyuan@gmail.com>
2025-06-19 06:38:39 -0700

    add router to /users

---
+++ api/user.go
@@ -0,0 +1,85 @@
+package api
+
+import (
+	"database/sql"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5/pgconn"
+	db "github.com/xianfengyuan/simplebank/db/sqlc"
+	"github.com/xianfengyuan/simplebank/util"
+)
+
+type createUserRequest struct {
+	Username string `json:"username" binding:"required,alphanum"`
+	Password string `json:"password" binding:"required,min=6"`
+	FullName string `json:"full_name" binding:"required"`
+	Email    string `json:"email" binding:"required,email"`
+}
+
+func (server *Server) createUser(ctx *gin.Context) {
+	var req createUserRequest
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	hashedPassword, _ := util.HashedPassword(req.Password)
+	arg := db.CreateUserParams{
+		Username:       req.Username,
+		HashedPassword: hashedPassword,
+		FullName:       req.FullName,
+		Email:          req.Email,
+	}
+
+	user, err := server.store.CreateUser(ctx, arg)
+	if err != nil {
+		if pgerr, ok := err.(*pgconn.PgError); ok {
+			switch pgerr.Code {
+			case "23503", "23505":
+				ctx.JSON(http.StatusForbidden, errorResponse(err))
+				return
+			}
+		}
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+	ctx.JSON(http.StatusOK, user)
+}
+
+type getUserRequest struct {
+	Username string `json:"username" binding:"required,alphanum"`
+}
+
+type userResp struct {
+	Username          string `json:"username" binding:"`
+	FullName          string `json:"full_name"`
+	Email             string `json:"email"`
+	PasswordChangedAt string `json:"password_changed_at"`
+	CreatedAt         string `json:"created_at"`
+}
+
+func (server *Server) getUser(ctx *gin.Context) {
+	var req getUserRequest
+	if err := ctx.ShouldBindUri(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+	user, err := server.store.GetUser(ctx, req.Username)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			ctx.JSON(http.StatusNotFound, errorResponse(err))
+			return
+		}
+
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+	ctx.JSON(http.StatusOK, userResp{
+		Username:          user.Username,
+		FullName:          user.FullName,
+		Email:             user.Email,
+		PasswordChangedAt: user.PasswordChangedAt.Time.String(),
+		CreatedAt:         user.PasswordChangedAt.Time.String(),
+	})
+}
